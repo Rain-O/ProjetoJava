76f6094e0b2ce1f62c52200bcfa2125eb0d1d375
Rian Freitas <Rian Freitas>
2025-08-12 16:02:39 -0300

    Removendo printl com variaveis

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class JavaProject {
+
+    public static void main(String[] args) {
+        String home;
+        int house = 7;
+        home = "Ola mundo do";
+        //Testando o programa
+        System.out.println("Hello World, aprendendo novamente java");
+    }
+
+
 }
